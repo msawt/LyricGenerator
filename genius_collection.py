3c9b55d6bdd1972d5c93689aa854e5e4c2e66b19
mileshong1 <milesh1@uci.edu>
2021-01-21 13:58:16 -0800

    Redesign to bypass name issue

--- genius_collection.py
+++ genius_collection.py
@@ -14,10 +14,9 @@ def create_lyrics_json_file(artist_name):
     artist = genius.search_artist(artist_name, sort="title")
     artist.save_lyrics()
 
-def parse_json_to_csv(artist_name):
-    artist_name = artist_name.replace(" ", "")
-    csv_file = "Lyrics_" + artist_name + "_parsed.csv"
-    json_file = "Lyrics_" + artist_name + ".json"
+def parse_json_to_csv(file_name):
+    csv_file = file_name.replace(".json", ".csv")
+    json_file = file_name
 
     with open(json_file) as file:
         data = json.load(file)
@@ -38,18 +37,21 @@ def parse_json_to_csv(artist_name):
 
 if __name__ == "__main__":
     #type the artists you want to extract
-    artists = ["The Weeknd", "Daryl Hall & John Oates", "PREP"]
+    artists = ["PREP", "HONNE"]
 
     genius.skip_non_songs = True
     genius.remove_section_headers = True
     #will have to adjust this so we do not have duplicate songs
     genius.excluded_terms = ["(Remix)", "(london session)", "(dream edit)", "(Late Night Version)", "(Aminé Remix)", 
-    "(The Lonely Players Club)", "(Livingston Session)", "(Duet Version)", "(Live)", "(Session)", "(Demo)", "(A Cappella)", "(Leak)", "(Version)"]
+    "(The Lonely Players Club)", "(Livingston Session)", "(Duet Version)", "(Live)", "(Session)", "(Demo)", "(A Cappella)", "(Leak)", "(Version)", "(Remastered)", "(mix)"]
 
     for artist in artists:
         print("Working on {0}...".format(artist))
-
         create_lyrics_json_file(artist)
-        parse_json_to_csv(artist)
-
         print("Done.")
+
+    for filename in os.listdir(os.getcwd()):
+        if filename.endswith(".json"):
+            print("Parsing {0}...".format(filename))
+            parse_json_to_csv(filename)
+            print("Done.")
